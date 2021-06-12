bb99250da245b2e2e31326107e8b99bc5752631b
Vasily93 <markovavasily@gmail.com>
2021-06-12 13:43:00 -0700

    init setup

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('lets start building timer app')
+
+new class Timer {
+    constructor() {
+
+    }
+    start() {
+
+    }
+    pause() {
+
+    }
+    onDurationChange() {
+
+    }
+    tick() {
+        
+    }
+}
