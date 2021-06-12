bc7b5f6e9cdf5482f3393c6a1d92ad46830997cd
Vasily93 <markovavasily@gmail.com>
2021-06-12 16:30:05 -0700

    enabled start and tick methods to start count and count down to zero

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-console.log('lets start building timer app')
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
 
-new class Timer {
-    constructor() {
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+        // this.durationInput.addEventListener('change', this.tick)
+    }
 
-    }
-    start() {
-
-    }
-    pause() {
-
-    }
-    onDurationChange() {
-
-    }
-    tick() {
-        
-    }
+        start = () => {
+            setInterval(this.tick, 1000)
+        }
+        pause = () => {
+            console.log('Pause timer')
+        }
+        onDurationChange() {
+            
+        }
+        tick = () => {
+            let seconds = parseInt(this.durationInput.value);
+            if(seconds > 0 ) {
+                seconds--;
+                this.durationInput.value = seconds;
+            } else {
+                console.log('Finish!!')
+            }
+        }
+    
 }
+
+const startB = document.getElementById('start');
+const pauseB = document.getElementById('pause');
+const durationIn = document.getElementById('duration');
+
+const newTimer = new Timer(durationIn, startB, pauseB);
+
+
+
+console.log(startB, pauseB);
