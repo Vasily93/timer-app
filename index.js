c37332442d26fb60cc21de9d80158f618f77ae12
Vasily93 <markovavasily@gmail.com>
2021-06-17 14:46:53 -0700

    finished all js logic with timer start and pause

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const startB = document.getElementById('start');
 const pauseB = document.getElementById('pause');
 const durationIn = document.getElementById('duration');
 
+const circle = document.querySelector('circle');
+const perimiter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimiter); 
+
+let duration;
 const newTimer = new Timer(durationIn, startB, pauseB, {
-    onStart() {
-        console.log('Timer Started!')
+    onStart(totalDuration) {
+        duration = totalDuration;
     },
-    onTick() {
-        console.log('Ticked!');
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+            perimiter * timeRemaining / duration - perimiter
+        )
     },
     onComplete() {
         console.log('Timer completed!')
--- timer.js
+++ timer.js
@@ -11,28 +11,24 @@ class Timer {
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
-        // this.durationInput.addEventListener('change', this.tick)
     }
 
         start = () => {
             if(this.onStart) {
-                this.onStart();
+                this.onStart(this.timeRemaining);
             }
             this.tick(); //calling 1st second tick right after the click
-            this.interval = setInterval(this.tick, 1000);
+            this.interval = setInterval(this.tick, 20);
         }
         pause = () => {
             clearInterval(this.interval);
         }
-        onDurationChange() {
-            
-        }
         tick = () => {
             if(this.timeRemaining > 0 ) {
                 if(this.onTick) {
-                    this.onTick();
+                    this.onTick(this.timeRemaining);
                 }
-                this.timeRemaining = this.timeRemaining - 1; //using get and set opertators
+                this.timeRemaining = this.timeRemaining - .02; //using get and set opertators
             } else {
                 this.pause();
                 if(this.onComplete) {
@@ -44,7 +40,7 @@ class Timer {
             return parseFloat(this.durationInput.value);
         }
         set timeRemaining(seconds) {
-            this.durationInput.value = seconds;
+            this.durationInput.value = seconds.toFixed(2); //rounding up float num to 2 decimels
         }
     
 }
